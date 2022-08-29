7b47a34c6cbfde39d45ed5e9124044ccc752599f
beniza <beniza@gmail.com>
2022-08-29 18:18:36 +0530

    add more info on branches

--- src/test.py
+++ src/test.py
@@ -1,2 +1,16 @@
+from ast import main
 import unittest
 
+# sumi_learn (main)
+#   |_ src
+#   |  |_ test.py
+#   |  |_ main.py
+#   |_ LICENSE
+#   |_ README.md
+
+# sumi_learn (ben)
+#   |_ src
+#   |  |_ test.py
+#   |  |_ main.py
+#   |_ LICENSE
+#   |_ README.md
